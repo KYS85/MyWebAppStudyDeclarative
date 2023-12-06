6f2e14476cfaf293bc0b773274dfc80946927c76
KYS85 <y.s.kostya@gmail.com>
2023-12-06 10:33:56 +0300

    New

---
+++ ViewController/src/view/MyBean.java
@@ -0,0 +1,13 @@
+package view;
+
+import javax.faces.event.ActionEvent;
+
+public class MyBean {
+    public MyBean() {
+    }
+
+    public void myMethod(ActionEvent actionEvent) {
+        // Add event code here...
+        System.out.println("!!! Hello World !!!");
+    }
+}
